e5c7f9d7ba63b28effe4ab2e8c0f8f25499848a1
JulianT3370 <jt5052@gmail.com>
2025-01-23 15:23:52 -0500

    configuraciones de puertos

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    # Obtén el puerto de la variable de entorno (DigitalOcean lo define automáticamente)
+    port = int(os.environ.get("PORT", 8080))  # Usa 8080 por defecto si no se encuentra la variable PORT
+    app.run(debug=True, host="0.0.0.0", port=port)  # Asegúrate de usar host="0.0.0.0" para aceptar conexiones externas
